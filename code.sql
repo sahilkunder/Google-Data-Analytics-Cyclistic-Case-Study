c913341436aea9bde942241c400e82137ebd2cbc
sahilkunder <118273597+sahilkunder@users.noreply.github.com>
2022-11-21 13:36:33 +0800

    Create code.sql

---
+++ code.sql
@@ -0,0 +1,21 @@
+WITH
+combined_year_data AS (
+
+SELECT *  
+FROM `coursera-trial-364606.Divvy_Bike_Data.202101_divvy_tripdata` 
+UNION ALL
+SELECT *
+FROM `coursera-trial-364606.Divvy_Bike_Data.202102_divvy_tripdata` 
+UNION ALL
+SELECT *
+FROM `coursera-trial-364606.Divvy_Bike_Data.202103_divvy_tripdata` 
+UNION ALL
+SELECT *
+FROM `coursera-trial-364606.Divvy_Bike_Data.202104_divvy_tripdata` 
+UNION ALL
+SELECT *
+FROM `coursera-trial-364606.Divvy_Bike_Data.202105_divvy_tripdata` 
+)
+
+SELECT *
+FROM combined_year_data
